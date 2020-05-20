f1b18c704ece46e8e4c5caa40091a6710d3c8367
Aceki <aceki320@gmail.com>
2020-05-20 12:19:36 +0500

    feat: Add files to repository.

---
+++ UnitedWorld/MyForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace UnitedWorld
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mapBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mapBox
+            // 
+            this.mapBox.Image = global::UnitedWorld.Properties.Resources.map;
+            this.mapBox.Location = new System.Drawing.Point(0, 0);
+            this.mapBox.Name = "mapBox";
+            this.mapBox.Size = new System.Drawing.Size(1280, 720);
+            this.mapBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mapBox.TabIndex = 0;
+            this.mapBox.TabStop = false;
+            // 
+            // MyForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.mapBox);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MyForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mapBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mapBox;
+    }
+}
+
---
+++ UnitedWorld/MyForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnitedWorld
+{
+    public partial class MyForm : Form
+    {
+        public MyForm()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            mapBox.MouseWheel += (sender, args) =>
+            {
+                var position = new Point(args.X + mapBox.Location.X, args.Y + mapBox.Location.Y);
+                var zoomRatio = (args.Delta < 0 ? 0.95 : 1.05);
+                mapBox.Size = new Size((int)(mapBox.Bounds.Width * zoomRatio), (int)(mapBox.Height * zoomRatio));
+                mapBox.Location = new Point((int)((mapBox.Bounds.X - position.X) * zoomRatio + position.X), (int)((mapBox.Bounds.Y - position.Y) * zoomRatio + position.Y));
+            };
+        }
+    }
+}
